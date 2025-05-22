10d5de502c686af879b1cc2a2247606a42aafb8a
Katarina Floyd <katarina.floyd@lhind.dlh.de>
2025-05-22 15:16:51 +0200

    arrays

--- src/07_arrays/reverseArray.js
+++ src/07_arrays/reverseArray.js
@@ -7,6 +7,11 @@
  */
 function reverseArray(arr) {
     // Your code here
+    let reversedArray = [];
+    for (let i = arr.length - 1; 0 <= i; i--) {
+        reversedArray.push(arr[i]);
+    }
+    return reversedArray;
 }
 
 module.exports = reverseArray;
